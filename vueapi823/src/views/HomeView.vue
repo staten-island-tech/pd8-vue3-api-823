9f2f3e67a8b701817cf1eff40f3fe513f959fe6b
GlassPainter <ckwong6599@gmail.com>
2023-04-17 14:02:13 -0400

    Imported URL
    Need to look at lifecycle hooks

--- vueapi823/src/views/HomeView.vue
+++ vueapi823/src/views/HomeView.vue
@@ -3,16 +3,15 @@
 </template>
 
 <script setup>
-/*
------------------------------------------------
 import { ref, onMounted } from 'vue'
 const test = ref('')
 async function getData() {
-  let res = await fetch()
+  let res = await fetch(
+    'https://data.cityofnewyork.us/resource/nu7n-tubp.json?$query=SELECT%0A%20%20%60animalname%60%2C%0A%20%20%60animalgender%60%2C%0A%20%20%60animalbirth%60%2C%0A%20%20%60breedname%60%2C%0A%20%20%60zipcode%60%2C%0A%20%20%60licenseissueddate%60%2C%0A%20%20%60licenseexpireddate%60%2C%0A%20%20%60extract_year%60'
+  )
+  let data = res.json()
 }
-L1 6:30
------------------------------------------------
-*/
+// L1 9:10
 </script>
 
 <style scoped></style>
