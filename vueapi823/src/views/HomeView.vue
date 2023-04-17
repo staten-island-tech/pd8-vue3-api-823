aca686644202224c86873706fd95996c7a0cb931
GhTmp1A <stanbutter4@gmail.com>
2023-04-17 14:01:33 -0400

    Switch to pokemon api.
    Switched to pokemon api for the tutorials for now and created new branch.

--- vueapi823/src/views/HomeView.vue
+++ vueapi823/src/views/HomeView.vue
@@ -3,16 +3,14 @@
 </template>
 
 <script setup>
-/*
------------------------------------------------
 import { ref, onMounted } from 'vue'
 const test = ref('')
 async function getData() {
-  let res = await fetch()
+  let res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+  let data = await res.json()
+  pokemon.values = data.results
 }
-L1 6:30
------------------------------------------------
-*/
+//L1 6:51
 </script>
 
 <style scoped></style>
