f5737893bcdcf62b7e02cb0e5558ecbe7e926f36
Christopher Kwong <ckwong6599@gmail.com>
2023-04-18 14:02:13 -0400

    Lesson 2

---
+++ vueapi823/src/components/DataCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <h2>{{ test.animalname }}</h2>
+    <h2>{{ id }}</h2>
+  </div>
+</template>
+
+<script>
+const props = {
+  data: Object,
+  id: Number
+}
+</script>
+
+<style lang="scss" scoped></style>
--- vueapi823/src/views/HomeView.vue
+++ vueapi823/src/views/HomeView.vue
@@ -9,9 +9,23 @@ async function getData() {
   let res = await fetch(
     'https://data.cityofnewyork.us/resource/nu7n-tubp.json?$query=SELECT%0A%20%20%60animalname%60%2C%0A%20%20%60animalgender%60%2C%0A%20%20%60animalbirth%60%2C%0A%20%20%60breedname%60%2C%0A%20%20%60zipcode%60%2C%0A%20%20%60licenseissueddate%60%2C%0A%20%20%60licenseexpireddate%60%2C%0A%20%20%60extract_year%60'
   )
-  let data = res.json()
+  let data = await res.json()
+  test.value = data
 }
-// L1 9:10
+onMounted(() => {
+  getData()
+})
+// L2 1:50
 </script>
 
-<style scoped></style>
+<style scoped>
+.flexContainer {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  align-items: center;
+  margin: 30px auto;
+  width: 80vw;
+}
+</style>
