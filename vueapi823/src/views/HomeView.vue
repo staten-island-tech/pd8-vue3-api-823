7c3439279b47c914ec93b427ebd95545fdfd0916
GhTmp1A <stanbutter4@gmail.com>
2023-04-19 14:01:35 -0400

    Need to recreate code for dogData.vue

--- vueapi823/src/views/HomeView.vue
+++ vueapi823/src/views/HomeView.vue
@@ -1,16 +1,29 @@
 <template>
-  <div></div>
+  <div class="container">
+    <PokemonCard
+      v-for="(monster, index) in test"
+      :key="monster.name"
+      :id="index + 1"
+      :pokemon="monster"
+    ></PokemonCard>
+  </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
+import dogData from '../components/icons/dogData.Vue'
 const test = ref('')
 async function getData() {
-  let res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+  let res = await fetch(
+    'https://data.cityofnewyork.us/resource/nu7n-tubp.json?$query=SELECT%0A%20%20%60animalname%60%2C%0A%20%20%60animalgender%60%2C%0A%20%20%60animalbirth%60%2C%0A%20%20%60breedname%60%2C%0A%20%20%60zipcode%60%2C%0A%20%20%60licenseissueddate%60%2C%0A%20%20%60licenseexpireddate%60%2C%0A%20%20%60extract_year%60'
+  )
   let data = await res.json()
-  pokemon.values = data.results
+  test.value = data.results
 }
-//L1 6:51
+onMounted(() => {
+  getData()
+})
+//L2 3:34
 </script>
 
 <style scoped></style>
