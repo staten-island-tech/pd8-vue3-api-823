58f59c063a8bc48c2dc0c6d9e15524b646dbb4cb
GhTmp1A <stanbutter4@gmail.com>
2023-04-25 13:50:56 -0400

    New link found
    Advanced filtering needs to be figured out with new link.

---
+++ vueapi823/src/views/dogData.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h1>{{ animal.animalname }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      animal: {}
+    }
+  },
+  mounted: async function () {
+    await this.getDog()
+  },
+  methods: {
+    getDog: async function () {
+      // let rest = "https://data.cityofnewyork.us/resource/nu7n-tubp.json?$query=SELECT`animalname`,`animalgender`,`animalbirth`,`breedname`,`zipcode`,`licenseissueddate`,`licenseexpireddate`,`extract_year`WHERE (`key` IN (""))"
+
+      let res = await fetch(
+        `https://data.cityofnewyork.us/resource/nu7n-tubp.json?$query=SELECTanimalname,WHERE(keyIN ("value"))`
+      )
+      let data = await res.json()
+      console.log(data)
+      this.animal = data
+    }
+  },
+  mounted() {
+    this.getDog()
+  }
+}
+</script>
+
+<style scoped></style>
